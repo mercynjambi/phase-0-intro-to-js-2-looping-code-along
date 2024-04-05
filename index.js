fb15392f67624057aad7cd41b660d0b332c244d7
mercynjambi <njambim695@gmail.com>
2024-04-05 23:36:34 +0300

    comleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  
+  const messages = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+  console.log(messages);
+  
+
+  function countDown(number){
+    while (number >=0){
+        console.log(number);
+        number--;
+    }
+      }
+countDown(10);
